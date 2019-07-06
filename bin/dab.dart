62a3cba0a742085c7b69cab5c38441127d7baec4
mockturtl <mockturtl@gmail.com>
2019-07-06 00:32:35 -0400

    wip messing with json->yaml pubspec conversion

--- bin/dab.dart
+++ bin/dab.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
 
 import 'package:dab/dab.dart';
+import 'package:pub_semver/pub_semver.dart' show VersionConstraint;
+import 'package:pubspec_parse/pubspec_parse.dart';
+import 'package:yamlicious/yamlicious.dart' show writeYamlString;
 
 main(List<String> arguments) async {
   if (arguments.isEmpty) {
@@ -11,4 +14,16 @@ main(List<String> arguments) async {
   print('Fetching $pkg...');
   var res = (await latest(pkg));
   print('${res.name} ${res.version}');
+
+  var p = Pubspec.parse(await File('pubspec.yaml').readAsString());
+  print('${p.name} ${p.dependencies}');
+  p.dependencies.putIfAbsent(
+      res.name,
+      () => HostedDependency(
+          version: VersionConstraint.parse('^${res.version}')));
+  print('${p.name} ${p.dependencies}');
+
+  var g = File('test.yaml').openWrite();
+
+  writeYamlString(p.toJson(), g);
 }
